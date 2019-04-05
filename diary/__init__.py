25671f2c705712d3a4b5f5565699565bef753aa6
Tuomas <tuomas.koivuaho@gmail.com>
2019-04-05 12:22:47 +0300

    db path fixe

--- diary/__init__.py
+++ diary/__init__.py
@@ -10,7 +10,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY="dev",
-        SQLALCHEMY_DATABASE_URI="sqlite:///db/pwp.db"),
+        SQLALCHEMY_DATABASE_URI="sqlite:///../db/pwp.db",
         SQLALCHEMY_TRACK_MODIFICATIONS=False
     )
     
