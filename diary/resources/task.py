bf9ff01fcf2dcb3732f42164738a6ec873e9bbfc
mamietti <mauri.miettinen13@gmail.com>
2019-04-07 23:56:49 +0300

    Added task and task collection resources (untested)

--- diary/resources/task.py
+++ diary/resources/task.py
@@ -1,13 +1,65 @@
+import json
+from datetime import datetime
+
+from flask import Response, url_for, request
 from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
+
+from diary import db
+from diary.models import ScheduleTask, Task
+from diary.utils import MasonBuilder, DiaryBuilder, MIMETYPE, TIME_FORMAT
 
 class TaskResource(Resource):
     
-    def get(self):
-        raise NotImplementedError
-    
-    def patch(self):
-        raise NotImplementedError
+    def get(self, schedule_id, task_id):
+        task_item = Task.query.filter_by(id=task_id).first()
+        if task_item is None:
+            return DiaryBuilder.create_error_response(404, 'Task does not exist')
+        body = DiaryBuilder()
+        body['name'] = task_item.name
+        body['priority'] = task_item.priority
+        body['goal'] = task_item.goal
+        body['result'] = task_item.result
+        body.add_namespace()
+        body.add_control('profile', '/profiles/task/')
+        body.add_control('collection', url_for('.taskcollection', schedule_id=schedule_id))
+        body.add_control_add_task(schedule_id)
+        body.add_control_edit_task(schedule_id,task_id)
+        body.add_control_delete_task(schedule_id, task_id)
+        return Response(json.dumps(body, indent=4), mimetype=MIMETYPE)
 
-    def delete(self):
-        raise NotImplementedError
+    def patch(self,schedule_id, task_id):
+        if request.json is not None:
+            try:
+                name = request.json['name']
+                priority = int(request.json['priority'])
+                goal = request.json.get('goal',None)
+                result = request.json.get('result',None)
+            except KeyError:
+                return DiaryBuilder.create_error_response(400, 'Missing keys in payload')
+            except ValueError:
+                return DiaryBuilder.create_error_response(400, 'Duration must be integer')
+        else:
+            return DiaryBuilder.create_error_response(415, 'please json')
+        
+        task_item = Task.query.filter_by(id=task_id).first()
+        if task_item is None:
+            return DiaryBuilder.create_error_response(404, 'Task does not exist')
+        task_item.name = name
+        task_item.priority = priority
+        task_item.goal = goal
+        task_item.result = result
+        try:
+            db.session.commit()
+        except IntegrityError:
+            return DiaryBuilder.create_error_response(409, 'Task already exists')
+        else:
+            return Response(status=204)
 
+    def delete(self, task_id):
+        task_item = Task.query.filter_by(id=task_id).first()
+        if task_item is None:
+            return DiaryBuilder.create_error_response(404, 'Task does not exist')
+        db.session.delete(task_item)
+        db.session.commit()
+        return Response(status=204)
--- diary/resources/task_collection.py
+++ diary/resources/task_collection.py
@@ -1,8 +1,67 @@
+import json
+from datetime import datetime
+
+from flask import Response, url_for, request
 from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
+
+from diary import db
+from diary.models import ScheduleTask, Task
+from diary.utils import MasonBuilder, DiaryBuilder, MIMETYPE, TIME_FORMAT
 
 class TaskCollection(Resource):
-    def get(self):
-        raise NotImplementedError
+    def get(self, schedule_id):
+        query = ScheduleTask.query.filter_by(schedule_id=schedule_id).first()
+        if query is None:
+            return DiaryBuilder.create_error_response(404, 'Schedule does not exist')
+        body = DiaryBuilder()
+        body.add_namespace()
+        body.add_control('self', url_for('.taskcollection', schedule_id=schedule_id))
+        body.add_control('collection', url_for('.scheduleresource',schedule_id=schedule_id))
+        body.add_control('profile','/profiles/task/')
+        body.add_control_add_event(schedule_id)
+        body.add_control_items_in(schedule_id)
+        body.add_control_event_in(schedule_id)
+        task_list = Task.query.all()
+        items = []
+        for task_item in task_list:
+            item_dict = MasonBuilder( 
+                name=task_item.name,
+                priority=task_item.priority,
+                goal=task_item.goal,
+                result=task_item.result
+                )
+            item_dict.add_control(
+                'self',url_for('.taskresource',schedule_id=schedule_id,task_id=task_item.id))
+            item_dict.add_control('profile','/profiles/task/')
+            items.append(item_dict)
+        body['items'] = items
+        return Response(json.dumps(body, indent=4),status=200, mimetype=MIMETYPE)
     
-    def post(self):
-        raise NotImplementedError
+    def post(self, schedule_id):
+        if request.json is not None:
+            try:
+                name = request.json['name']
+                priority = int(request.json['priority'])
+                goal = request.json.get('goal',None)
+                result = request.json.get('result',None)
+            except KeyError:
+                return DiaryBuilder.create_error_response(400, 'Missing keys in payload')
+            except ValueError:
+                return DiaryBuilder.create_error_response(400, 'Duration must be integer')
+        else:
+            return DiaryBuilder.create_error_response(415, 'please json')
+        
+        task = Task(
+            name=name,
+            priority=priority,
+            goal=goal,
+            result=result
+        )
+        try:
+            db.session.add(task)
+            db.session.commit()
+        except IntegrityError:
+            return DiaryBuilder.create_error_response(409, 'Task already exists')
+        else:
+            return Response(201)
