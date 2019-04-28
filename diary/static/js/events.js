29424e872ca864582c38e64631f296a6e926d97c
Valtteri Erkkila <valtteri.erkkila@gmail.com>
2019-04-28 19:20:45 +0300

    HTML and JS for events and tasks

---
+++ diary/static/js/events.js
@@ -0,0 +1,50 @@
+'use strict';
+
+function updateRow(event){
+    let tbody = $(".resulttable tbody");
+    let tableRowHtml = "<tr>" + createTableCell(event.id, "id") +
+    createTableCell(event.name, "name") +
+    createTableCell(event.duration, "duration") +
+    createTableCell(event.value, "value") +
+    "<td>" + "<form method=\"DELETE\" action=\"/diary/schedules/"  + $.urlParam("schedule_id") +
+    "/events/" +  event.id + "/\">" +
+    "<input type=\"submit\"  onclick=\"deleteRow(this)\"value=\"&#x2718;\"></input>" +
+    "</form>" +
+    "</td></tr>";
+    tbody.append(tableRowHtml);
+}
+
+function createRows(body){
+    let events = body.items;
+    events.forEach(function(event) {
+        updateRow(event);
+    });
+    let tableRows = ($("tbody tr td[contentEditable=true]"));
+    tableRows.on("blur", function(){
+        let outData = {};
+        outData.name = $(this).parent().children(".name").html().trim();
+        outData.duration = $(this).parent().children(".duration").html().trim();
+        outData.note = $(this).parent().children(".note").html().trim();
+        let url = "http://localhost:5000/diary/schedules/" + $.urlParam("schedule_id") + "/events/" + $(this).parent().children(".id").html().trim() + "/";
+        $.ajax({
+            url:url,
+            method:"PUT",
+            data:JSON.stringify(outData),
+            contentType: MASON,
+            success: null
+        });
+    });
+}
+
+function refreshTable(body){
+    $(".resulttable tbody").empty();
+    getResource("http://localhost:5000/diary/schedules/" + $.urlParam("schedule_id") + "/events/", createRows);
+}
+
+$(document).ready(function (){
+    setNavLinks();
+    getQueryParams();
+    $("#new-post").attr("action", "http://localhost:5000/diary/schedules/" + $.urlParam("schedule_id") + "/events/")
+    getResource("http://localhost:5000/diary/schedules/" + $.urlParam("schedule_id") + "/events/", createRows);
+    submitForm(refreshTable);
+})
---
+++ diary/static/js/tasks.js
@@ -0,0 +1,51 @@
+'use strict';
+
+function updateRow(task){
+    let tbody = $(".resulttable tbody");
+    let tableRowHtml = "<tr>" + createTableCell(task.id, "id") + 
+    createTableCell(task.name, "name") +
+    createTableCell(task.priority, "priority") +
+    createTableCell(task.goal, "goal") +
+    createTableCell(task.result, "result") +
+    "<td>" + "<form method=\"DELETE\" action=\"/diary/schedules/"  + $.urlParam("schedule_id")  +
+    "/tasks/" +  task.id + "/\">" +
+    "<input type=\"submit\"  onclick=\"deleteRow(this)\"value=\"&#x2718;\"></input>" +
+    "</form>" +
+    "</td></tr>";
+    tbody.append(tableRowHtml);
+}
+
+function createRows(body){
+    let tasks = body.items;
+    tasks.forEach(function(task) {
+        updateRow(task);
+    });
+    let tableRows = ($("tbody tr td[contentEditable=true]"));
+    tableRows.on("blur", function(){
+        var outData = new Object();
+        outData.name = $(this).parent().children(".name").html().trim();
+        outData.priority = parseInt($(this).parent().children(".priority").html().trim());
+        outData.goal = $(this).parent().children(".goal").html().trim();
+        let url = "http://localhost:5000/diary/schedules/" + $.urlParam("schedule_id") + "/tasks/" + $(this).parent().children(".id").html().trim() + "/";
+        $.ajax({
+            url:url,
+            method:"PUT",
+            data:JSON.stringify(outData),
+            contentType: MASON,
+            success: null
+        });
+    });
+}
+
+function refreshTable(body){
+    $(".resulttable tbody").empty();
+    getResource("http://localhost:5000/diary/schedules/" + $.urlParam("schedule_id") + "/tasks/", createRows);
+}
+
+$(document).ready(function (){
+    setNavLinks();
+    getQueryParams();
+    $("#new-post").attr("action", "http://localhost:5000/diary/schedules/" + $.urlParam("schedule_id") + "/tasks/");
+    getResource("http://localhost:5000/diary/schedules/" + $.urlParam("schedule_id") + "/tasks/", createRows);
+    submitForm(refreshTable);
+})
